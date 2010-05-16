6258a69959fb335aed554dc49d66e5fd3e751b48
Bastian Kennel <bastian@simpsus>
2010-05-16 17:55:08 +0200

    introducing the onvista plugin as a copy of the helloworld plugin

---
+++ stocktracker/plugins/onvista/onvista_plugin.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import gtk
+
+class OnvistaPlugin():
+    
+    def __init__(self):
+        self.menu_item = gtk.MenuItem("Hello World Plugin")
+        self.menu_item.connect('activate', self.on_menu_clicked)
+
+    def activate(self):
+        self.api.add_menu_item(self.menu_item, 'Tools')
+                
+    def deactivate(self):
+        self.api.remove_menu_item(self.menu_item)
+        
+    def on_menu_clicked(self, widget):
+        d = gtk.MessageDialog(parent=None, flags=0, type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_NONE, message_format=None)
+        d.set_markup("This is the onvista search plugin")
+        d.run()
+        d.destroy()
