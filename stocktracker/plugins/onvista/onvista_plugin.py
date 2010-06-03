cf6f908f8e71b03b34fc5667385fe3c9945372e4
Bastian Kennel <bastian@simpsus>
2010-06-03 13:27:48 +0200

    onvist plugin is now naked again

--- stocktracker/plugins/onvista/onvista_plugin.py
+++ stocktracker/plugins/onvista/onvista_plugin.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import scrapy.core.manager as manager
-from stocktracker.engine.finanzpartner.finanzpartner.spiders import onvistaSpider
-
 class OnvistaPlugin():
     configurable = False
     
     def __init__(self):
         self.name = 'onvista.de'
-        self.manager = manager.scrapymanager
 
     def activate(self):
         self.api.register_datasource(self, self.name)
@@ -19,10 +15,3 @@ class OnvistaPlugin():
         
     def search(self, searchstring, callback):
         print "searching using ", self.name
-        spider = onvistaSpider.OnvistaSpider()
-        onvistaSpider.SPIDER = spider
-        spider.setCallback(callback)
-        spider.schedule_search(searchstring)
-        self.manager.configure()
-        self.manager.queue.append_spider(spider)
-        self.manager.start()
