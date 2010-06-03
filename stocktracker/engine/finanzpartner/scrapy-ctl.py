4756d57e9bf105bd2bd9a0ff15787ac4627dfa96
Bastian Kennel <bastian@simpsus>
2010-06-03 16:25:04 +0200

    20 lines and already better than the FCUKING scrapy spider

--- stocktracker/engine/finanzpartner/scrapy-ctl.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import os
-os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'stocktracker.engine.finanzpartner.finanzpartner.settings')
-
-from scrapy.command.cmdline import execute
-execute()
--- stocktracker/plugins/onvista/onvista_plugin.py
+++ stocktracker/plugins/onvista/onvista_plugin.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from BeautifulSoup import BeautifulSoup 
+import re
+    
 
 class OnvistaPlugin():
     configurable = False
@@ -13,5 +16,32 @@ class OnvistaPlugin():
     def deactivate(self):
         self.api.deregister_datasource(self, self.name)
         
+    def curlURL(self, url):
+        url = "'" + url.replace("'", "'\\''") + "'"
+        print url
+        import os, tempfile
+        fd, tempname = tempfile.mkstemp(prefix='scrape')
+        command = 'curl --include --insecure --silent ' + url
+        print "Command: ", command
+        os.system(command + ' > ' + tempname)
+        reply = open(tempname).read()
+        os.remove(tempname)
+        return reply
+        
     def search(self, searchstring, callback):
         print "searching using ", self.name
+        search_URL ='http://www.onvista.de/suche.html?TARGET=snapshot&ID_TOOL=FUN&SEARCH_VALUE='+searchstring
+        soup = BeautifulSoup(self.curlURL(search_URL))
+        linkTags = soup.findAll(attrs={'href' : re.compile('http://fonds\\.onvista\\.de/snapshot\\.html\?ID_INSTRUMENT=\d+')})
+        links = [tag['href'] for tag in linkTags]
+        erg = []
+        for link in links:
+            snapshot = self.curlURL(link)
+            ssoup = BeautifulSoup(snapshot)
+            name = ssoup.html.body.find('div', {'id':'ONVISTA'}).find('table','RAHMEN').tr.find('td','WEBSEITE').find('div','content').h2.contents[0]
+            print name
+        
+if __name__ == "__main__":
+    plugin = OnvistaPlugin()
+    searchstring = "multi"
+    plugin.search(searchstring, lambda:None)
