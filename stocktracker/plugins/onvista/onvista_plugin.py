8b71824b399bab3d5c59d034d3fa6379f3c31105
wolfer <wsteitz@gmail.com>
2010-06-13 19:12:33 +0200

    pass the staffelholz to basti

--- stocktracker/plugins/onvista/onvista_plugin.py
+++ stocktracker/plugins/onvista/onvista_plugin.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from BeautifulSoup import BeautifulSoup 
-from BeautifulSoup import NavigableString
-import re
+from BeautifulSoup import BeautifulSoup, NavigableString
 from datetime import datetime 
-import urllib, threading
+import threading, re
 from Queue import Queue
+import urllib
+import urllib2
 
     
 TYPE = 0
@@ -13,20 +13,11 @@ def to_float(s):
     return float(s.replace('.','').replace(',','.'))
 
 def to_datetime(date, time):
-    return datetime.strptime(date+time, "%d.%m.%Y%H:%M:%S")
+    return datetime.strptime(date+time, "%d.%m.%H:%M:%S")
 
-def curlURL(url):
-    url = "'" + url.replace("'", "'\\''") + "'"
-    import os, tempfile
-    #make a temp file to store the data in
-    fd, tempname = tempfile.mkstemp(prefix='scrape')
-    command = 'curl --include --insecure --silent ' + url
-    #download with curl into the tempfile
-    os.system(command + ' > ' + tempname)
-    #read, delete and return
-    reply = open(tempname).read()
-    os.remove(tempname)
-    return reply
+
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
 
 class FileGetter(threading.Thread):
@@ -40,7 +31,7 @@ class FileGetter(threading.Thread):
  
     def run(self):
         try:
-            self.result = curlURL(self.url)
+            self.result = opener.open(self.url)
             print "Downloaded ", self.url
         except IOError:
             print "Could not open document: %s" % self.url
@@ -70,6 +61,7 @@ def get_files(files):
     cons_thread.join()
     return finished
 
+
 class OnvistaPlugin():
     configurable = False
     
@@ -82,108 +74,69 @@ class OnvistaPlugin():
                         
     def deactivate(self):
         self.api.deregister_datasource(self, self.name)
-        
-    def search(self, searchstring, isIsin = False):
+    
+    def search(self, searchstring):
         #blacklist to filter table rows that do not contain a price
-        search_URL ='http://www.onvista.de/suche.html?TARGET=snapshot&ID_TOOL=FUN&SEARCH_VALUE='+searchstring
-        links = []
-        print "alive"
-        if isIsin:
-            links = [search_URL]
-        else:
-            soup = BeautifulSoup(curlURL(search_URL))
-            #all the tags that lead to a snapshot page on the search result page
-            linkTags = soup.findAll(attrs={'href' : re.compile('http://fonds\\.onvista\\.de/snapshot\\.html\?ID_INSTRUMENT=\d+')})
-            links = [tag['href'] for tag in linkTags]
-            #print "Found ", len(links)
-            #print "Calling FileGetter Queue to download"
-        content = get_files(links)
-        kursLinks = []
-        for cont in content:
-            #print "Fetching ", links.index(link)+1, "/", len(links)
-            #snapshot = self.curlURL(link)
-            #ssoup = BeautifulSoup(snapshot)
-            ssoup = BeautifulSoup(cont)
-            #the base content area containing everything of importance
-            base = ssoup.html.body.find('div', {'id':'ONVISTA'}).find('table','RAHMEN').tr.find('td','WEBSEITE').find('div','content')
-            name = base.h2.contents[0]
-            isin = base.find('table','hgrau1').tr.td.find('table','weiss').findAll('tr','hgrau2')[1].findAll('td')[1].contents[0].replace('&nbsp;','')
-            # the KAG kurs is on the snapshot page
-            kagKursText = base.find('span','KURSRICHTUNG').findNextSibling('span').contents[0]
-            kagKursCurrency = kagKursText[-3:]
-            kagKurs = to_float(kagKursText[:-3].replace('&nbsp;',''))
-            kagDateText = base.find('div',{'id':'KURSINFORMATIONEN'}).find('div','sm').contents[0]
-            kagDateTimeBase = kagDateText.partition('(')[2].partition(';')[2]
-            kagDate = kagDateTimeBase.partition(',')[0]
-            kagTime = kagDateTimeBase.partition(';')[2].partition(')')[0]
-            date = to_datetime(kagDate, kagTime)
-            kagChange = base.findAll('table','hgrau1')[3].tr.td.find('table','weiss').findAll('tr','hgrau2')[1].findAll('td')[1]
-            if kagChange.span:
-                kagChange = kagChange.span
-            kagChange = to_float(kagChange.contents[0])
-            #print "Returning KAG"
-            # return the result with the kag price
-            yield ({'name':name,'isin':isin,'exchange':'KAG','price':kagKurs,
-                      'date':date,'currency':kagKursCurrency,
-                      'type':TYPE,'change':kagChange}
-                ,self)
-            #for the prices on the different stock exchanges, there is a detail page
-            kurslink = ssoup.find(attrs={'href':re.compile('http://fonds\\.onvista\\.de/kurse\\.html')})['href']
-            kursLinks.append(kurslink)
-        kursPages = get_files(kursLinks)
-        for kursPage in kursPages:
-            #kursPage = curlURL(kurslink)
-            kursSoup = BeautifulSoup(kursPage)
-            tableRows = kursSoup.find('table','weiss abst').findAll('tr')
-            for row in tableRows:
-                tds = row.findAll('td')
-                if len(tds)>0:
-                    exchangeTag = tds[0]
-                    if not exchangeTag.contents[0] in self.exchangeBlacklist:
-                        exchange = ""
-                        #print type(exchangeTag.contents[0])
-                        if not isinstance(exchangeTag.contents[0], NavigableString):
-                            exchange = exchangeTag.a.contents[0]
-                        else:
-                            exchange = exchangeTag.contents[0]
-                        currencyTag = tds[1]
-                        currency = currencyTag.contents[0]
-                        buyPrice = to_float(tds[3].contents[0])
-                        sellPrice = tds[6].contents[0]
-                        date = to_datetime(tds[8].contents[0], tds[9].contents[0])
-                        volume = tds[12].contents[0]
-                        change = tds[14]
-                        if change.span:
-                            change = change.span
-                        change = to_float(change.contents[0])
-                        #print name, isin, exchange, currency, buyPrice,sellPrice,day,timeOfDay,volume
-                        #print "Returning ",exchange
-                        yield ({'name':name,'isin':isin,'exchange':exchange,
-                                  'currency':currency,'price':buyPrice,'sell':sellPrice,
-                                  'date':date,'volume':volume,
-                                  'type':TYPE,'change':change}
-                            ,self)
+        #search_URL ="http://www.onvista.de/suche.html?TARGET=kurs&ID_TOOL=FUN&SEARCH_VALUE=%s" % (searchstring,)
+        page = opener.open("http://www.onvista.de/suche.html", urllib.urlencode({"TARGET": "kurse", "SEARCH_VALUE": searchstring,'ID_TOOL':'FUN' }))
+        soup = BeautifulSoup(page.read())
+        #all the tags that lead to a snapshot page on the search result page
+        linkTags = soup.findAll(attrs={'href' : re.compile('http://fonds\\.onvista\\.de/kurse\\.html\?ID_INSTRUMENT=\d+')})
+        links = [tag['href'] for tag in linkTags]
+        
+        for kursPage in get_files(links):
+            for item in self._parse_kurs_html(kursPage):
+                yield item
+        
+    def _parse_kurs_html(self, kursPage):
+        soup = BeautifulSoup(kursPage)
+        base = soup.html.body.find('div', {'id':'ONVISTA'}).find('table','RAHMEN').tr.find('td','WEBSEITE').find('div','content')
+        name = base.h2.contents[0]
+        print "NAME", name
+        isin = base.find('table','hgrau1').tr.td.find('table','weiss').findAll('tr','hgrau2')[1].findAll('td')[1].contents[0].replace('&nbsp;','')
+        print "ISIN", isin
+        for row in soup.find('table','weiss abst').findAll('tr'):
+            tds = row.findAll('td')
+            print len(tds)
+            if len(tds)>0:
+                exchangeTag = tds[0]
+                if not exchangeTag.contents[0] in self.exchangeBlacklist:
+                    exchange = ""
+                    #print type(exchangeTag.contents[0])
+                    if not isinstance(exchangeTag.contents[0], NavigableString):
+                        exchange = exchangeTag.a.contents[0]
+                    else:
+                        exchange = exchangeTag.contents[0]
+                    currency = tds[1].contents[0]
+                    price = to_float(tds[11].contents[0])
+                    #sellPrice = tds[6].contents[0]
+                    date = to_datetime(tds[8].contents[0], tds[9].contents[0])
+                    volume = tds[12].contents[0]
+                    change = tds[14]
+                    if change.span:
+                        change = change.span
+                    change = to_float(change.contents[0])
+                    yield {'name':name,'isin':isin,'exchange':exchange,'price':price,
+                      'date':date,'currency':currency,'volume':volume,
+                      'type':TYPE,'change':change}
                         
     def update_stocks(self, stocks):
-        print "inUpdate"
-        isins = [stock.isin for stock in stocks]
-        for isin in isins:
-            print "lol"
-            self.search(isin, True)
-        
-        
+        for isin in [stock.isin for stock in stocks]:
+            url = 'http://fonds.onvista.de/kurse.html?ISIN='+isin
+            res = self._parse_kurs_html(get_files([url])[0])
+            print res
+               
         
 if __name__ == "__main__":
-    
-    print "GO"
     
     class Stock():
         def __init__(self, isin):
             self.isin = isin
     
-    print "noch da"
     plugin = OnvistaPlugin()
-    plugin.update_stocks([Stock('LU0136412771'), Stock('LU0103598305')])
+    for res in  plugin.search('multi'):
+        print res
+    #plugin.update_stocks([Stock('LU0136412771'), Stock('LU0103598305')])
     
     #searchstring = "emerging"
     #for item in plugin.search(searchstring):
