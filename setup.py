a61f80a4f0bc465d14a3591115c715e9bafb7221
Bastian Kennel <simpsus@simpsus-desktop>
2010-07-30 09:53:53 +0200

    releasing 0.4.0 the radtour

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='stocktracker',
-    version='0.3',
+    version='0.4.0',
     license='GPL v3',
     author='wolfer',
     author_email='wsteitz@gmail.com',
