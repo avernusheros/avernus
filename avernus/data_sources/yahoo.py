0e0d06282f6001478b2c96616b8afa86e935923f
wolfer <wsteitz@gmail.com>
2010-12-09 11:22:04 +0100

    adapt to new yahoo finance search site

--- avernus/data_sources/yahoo.py
+++ avernus/data_sources/yahoo.py
@@ -8,7 +8,7 @@ from avernus import config
 from avernus.logger import Log
 from avernus.objects import stock
 
-TYPES = {'Fonds':stock.FUND, 'Aktie':stock.STOCK, 'Namensaktie':stock.STOCK, 'Vorzugsaktie':stock.STOCK}
+TYPES = {'Fonds':stock.FUND, 'Aktien':stock.STOCK, 'Namensaktie':stock.STOCK, 'Vorzugsaktie':stock.STOCK}
 
 class Yahoo():
     name = "yahoo"
@@ -18,8 +18,7 @@ class Yahoo():
     
     def __request(self, searchstring):
         try:
-            url = 'http://de.finsearch.yahoo.com/de/index.php?nm='+searchstring+'&tp=*&r=*&sub=Suchen'
-            print url
+            url = 'http://de.finance.yahoo.com/lookup/all?s='+searchstring          
             Log.info(url)
             return urlopen(url)
         except:
@@ -121,19 +120,19 @@ class Yahoo():
             return
         #1. beatifull soup does not like this part of the html file
         #2. remove newlines
-        my_massage = [(re.compile('OPTION VALUE=>---------------------<'), ''), \
-                      (re.compile('\n'), '')]
-        soup = BeautifulSoup(doc, markupMassage=my_massage)
-        for main_tab in soup.findAll('table', width="752"):
-            for table in main_tab.findAll('table', cellpadding='3', cellspacing='1',width='100%'):
-                for row in table('tr'):
+        #my_massage = [(re.compile('OPTION VALUE=>---------------------<'), ''), \
+        #              (re.compile('\n'), '')]
+        soup = BeautifulSoup(doc)#, markupMassage=my_massage)
+        for table in soup.findAll('table', summary="YFT_SL_TABLE_SUMMARY"):
+            for body in table('tbody'):
+                for row in body('tr'):
                     item = []
-                    for s in row('td', {'class':'yfnc_tabledata1'}, text=True):
+                    for s in row('td',text=True):
                         s = s.strip()
                         if s is not None and s!=unicode(''):
                             item.append(s)
-                    if len(item) == 12:
-                        item = self.__to_dict(item[:-2])
+                    if len(item) == 6:
+                        item = self.__to_dict(item)
                         if item is not None:
                             if (item['isin'],item['currency']) in self.yahoo_ids.keys():
                                 if not item['yahoo_id'] in self.yahoo_ids[(item['isin'],item['currency'])] :
@@ -143,6 +142,7 @@ class Yahoo():
                             yield (item, self)
         self.__save_yahoo_ids()
     
+    #not used
     def __parse_price(self, pricestring):
         if pricestring[-1] == '$':
             price = pricestring[:-1]
@@ -160,20 +160,21 @@ class Yahoo():
         return price - (price*100 / (100 + float(changestring.strip('%'))))
                     
     def __to_dict(self, item):
-        if not item[5] in TYPES:
+        if not item[4] in TYPES:
             return None
         res = {}
-        res['name']                   = item[0]
-        res['yahoo_id']               = item[1]
-        res['isin']                   = item[2]
-        res['wkn']                    = item[3]
-        res['exchange']               = item[4]
-        res['type']                   = TYPES[item[5]]
-        res['price'], res['currency'] = self.__parse_price(item[6])
-        #res['time']                   = item[7]  #only time not date
-        res['date']                   = datetime.utcnow().replace(year=2009)
-        res['change']                 = self.__parse_change(item[8], res['price'])
-        res['volume']                 = int(item[9].replace(",", ""))
+        res['name']     = item[1]
+        res['yahoo_id'] = item[0]
+        res['isin']     = item[2]
+        #res['wkn']     = item[3]
+        res['exchange'] = item[5]
+        res['type']     = TYPES[item[4]]
+        res['price']    = float(item[3].replace('.','').replace(',','.'))
+        res['currency'] = None
+        #res['time']    = item[7]  #only time not date
+        res['date']     = datetime.utcnow().replace(year=2009)
+        #res['change']   = self.__parse_change(item[8], res['price'])
+        #res['volume']   = int(item[9].replace(",", ""))
         return res
     
     def __load_yahoo_ids(self):
