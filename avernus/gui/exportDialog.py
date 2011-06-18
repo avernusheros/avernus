796c998d49b33cdba1353df135c8772602fa0b76
simpsus <bastian.kennel@gmail.com>
2011-06-18 15:16:43 +0200

    exporting of account transactions as csv. In such a cool way that they can be imported by the importer

--- avernus/gui/exportDialog.py
+++ avernus/gui/exportDialog.py
@@ -5,9 +5,13 @@ Created on 12 Jun 2011
 '''
 from avernus import config
 from avernus.controller import controller
+from avernus.gui import gui_utils
 from avernus.gui.csv_import_dialog import PreviewTree
 import gtk
 import os
+import pango
+import time
+import csv
 
 class ExportDialog(gtk.Dialog):
     
@@ -17,9 +21,9 @@ class ExportDialog(gtk.Dialog):
         gtk.Dialog.__init__(self, self.TITLE, None
                             , gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
                      (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,))
-        print "alive"
         self.account = account
         self.config = config.avernusConfig()
+        self.transactions = []
         self._init_widgets()
         
         response = self.run()
@@ -39,6 +43,7 @@ class ExportDialog(gtk.Dialog):
         folder = self.config.get_option('last_export_folder')
         if folder is not None:
             self.fcbutton.set_current_folder(folder)
+            self.folder = folder
         fileBox.pack_start(self.fcbutton, fill=True)
         accBox = gtk.HBox()
         vbox.pack_start(accBox, fill=False, expand=False)
@@ -62,7 +67,7 @@ class ExportDialog(gtk.Dialog):
         frame = gtk.Frame('Transactions')
         sw = gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
-        self.tree = PreviewTree()
+        self.tree = TransactionTree()
         frame.add(sw)
 
         sw.add(self.tree)
@@ -70,19 +75,78 @@ class ExportDialog(gtk.Dialog):
         self.show_all()
         
     def _on_file_set(self, button):
-        self.file = button.get_filename()
-        self.set_title(self.TITLE+' - '+os.path.basename(self.file))
-        self.config.set_option('last_export_folder', button.get_current_folder())
-        self.fcbutton.set_current_folder(button.get_current_folder())
-        print button.get_current_folder()
-        #self.tree.reload(self.importer.results)
+        self.folder = button.get_current_folder()
+        self.config.set_option('last_export_folder', self.folder)
+        
 
     def _on_account_changed(self, *args):
         self.b_account = True
         self.account = self.account_cb.get_model()[self.account_cb.get_active()][0]
+        self.tree.reload(self.account)
+        self.export_button.set_sensitive(True)
         
     def process_result(self, widget=None, response = gtk.RESPONSE_ACCEPT):
         if response == gtk.RESPONSE_ACCEPT:
-            print "ACCEPT!!"
+            # gather the transactions
+            self.transactions = self.tree.getExportTransactions()
+            # generate the filename
+            name = "avernusAccountExport_v1_"
+            name += self.account.name
+            name += "_" + str(int(time.time()))
+            name += ".csv"
+            path = os.path.join(self.folder, name)
+            with open(path, 'wb') as file:
+                writer = csv.writer(file)
+                for transaction in self.transactions:
+                    writer.writerow([transaction.date, transaction.amount, 
+                                     transaction.description, transaction.category])
         self.destroy()
+        
+class TransactionTree(gui_utils.Tree):
+    
+    OBJECT = 0
+    DATE = 1
+    AMOUNT = 2
+    DESC = 3
+    EXPORT = 4
+    
+    def __init__(self):
+        gui_utils.Tree.__init__(self)
+        self.set_rules_hint(True)
+        self.set_size_request(700,400)
+        self.model = gtk.ListStore(object, str, float, str, bool)
+        self.set_model(self.model)
+        
+        column, cell = self.create_check_column('export?', self.EXPORT)
+        cell.connect("toggled", self.on_toggled)
+        column, cell = self.create_column('date', self.DATE)
+        column.add_attribute(cell, 'foreground', 5)
+        cell.set_property('foreground-set', True)
+        column, cell = self.create_column('amount', self.AMOUNT, func=gui_utils.currency_format)
+        column.add_attribute(cell, 'foreground', 5)
+        cell.set_property('foreground-set', True)
+        column, cell = self.create_column('description', self.DESC)
+        column.add_attribute(cell, 'foreground', 5)
+        cell.set_property('foreground-set', True)
+        cell.props.wrap_mode = pango.WRAP_WORD
+        cell.props.wrap_width = 300
+        
+    def getExportTransactions(self):
+        erg = []
+        for line in self.model:
+            object = line[self.OBJECT]
+            export = line[self.EXPORT]
+            if export:
+                erg.append(object)
+        return erg
+        
+    def on_toggled(self, cellrenderertoggle, path):
+        self.model[path][self.EXPORT] = not self.model[path][self.EXPORT] 
+        
+    def reload(self, account):
+        self.clear()
+        self.account = account
+        model = self.get_model()
+        for trans in account:
+            model.append([trans, trans.date, trans.amount, trans.description, True])
         
