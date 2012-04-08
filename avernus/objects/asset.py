5d0f9f0b0a0b861a266e79566dec6be80e6b4354
bastian-kennel <>
2012-04-08 19:18:02 +0200

    so

---
+++ avernus/objects/asset.py
@@ -0,0 +1,27 @@
+from avernus.objects import Base
+from sqlalchemy import Column, Integer, String, Float, Date, Boolean, ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+class Asset(Base):
+    
+    __tablename__ = 'asset'
+    
+    discriminator = Column('type', String(20))
+    
+    __mapper_args__ = {'polymorphic_on': discriminator}
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    date = Column(Date)
+    isin = Column(String)
+    exchange = Column(String)
+    currency = Column(String)
+    price = Column(Float)
+    source = Column(String)
+    change = Column(Float)
+    
+class Stock(Asset):
+    
+    __tablename__ = 'stock'
+    __mapper_args__ = {'polymorphic_identity': 'stock'}
+    id = Column(Integer, ForeignKey('asset.id'), primary_key=True)
