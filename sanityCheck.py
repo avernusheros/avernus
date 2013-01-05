ae18edcb7b671882ad77a8207f0385b37f37ca37
simpsus <bastian.kennel@gmail.com>
2013-01-05 20:39:15 +0100

    a few more sanity checks for tables

--- sanityCheck.py
+++ sanityCheck.py
@@ -19,10 +19,14 @@ db_file = configs.get_option('database file', default=default_file)
 #building the database connection
 con = None
 # tables that do not need a sanity check
-omitted_tables = ['container']
+omitted_tables = ['container', 'source_info']
+# Reasons for omitting tables:
+#    container: author does not know what could be checked (empty type??)
+#    source_info: author does not know what this table stores
 checked_tables = []
 table_names = []
-expected_tables = ['asset', 'quotation']
+expected_tables = ['asset', 'quotation', 'stock', 'category_filter', 'source_info',
+                   'dimension_value', 'fund', 'position']
     
 try:
     con = db.connect(db_file)
@@ -31,7 +35,7 @@ try:
     cur = con.cursor()
     cur.execute('SELECT SQLITE_VERSION()')
     data = cur.fetchone()
-    logger.debug("SQLite Version: %s" % data)
+    logger.debug("SQLite Version: %s" % data['SQLITE_VERSION()'])
     cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tables = cur.fetchall()
     for table in tables:
@@ -79,6 +83,51 @@ try:
     if stock_sane:
         logger.info('Sanity Check for stock passed')
     checked_tables.append('stock')
+    # category_filter: the rule must not be empty and the category id must exist
+    logger.info("Santiy Check for category_filter")
+    category_filter_sane = True
+    cur.execute('SELECT * from category_filter')
+    for cf in cur.fetchall():
+        if cf['rule'] == '':
+            logger.error('category_filter %s has an empty rule' % cf['id'])
+            category_filter_sane = False
+    cur.execute('SELECT * from category_filter WHERE NOT EXISTS (select 1 from account_category where category_filter.category_id = account_category.id)')
+    for zombie in cur.fetchall():
+        logger.error('category_filter %s referrs to a non-existant category %s' % (cf['id'], cf['category_id']))
+        category_filter_sane = False
+    if category_filter_sane:
+        logger.info('Sanity Check for category_filter passed')
+    checked_tables.append('category_filter')
+    # dimension_value: the dimension_id must exist
+    logger.info('Sanity Check for dimension_value')
+    dimension_value_sane = True
+    cur.execute('SELECT * from dimension_value WHERE NOT EXISTS (select 1 from dimension where dimension_value.dimension_id = dimension.id)')
+    for zombie in cur.fetchall():
+        logger.error('dimension_value %s referrs to a non-existant dimension %s' % (zombie['id'], zombie['dimension_id']))
+        dimension_value_sane = False
+    if dimension_value_sane:
+        logger.info('Sanity Check for dimension_value passed')
+    checked_tables.append('dimension_value')
+    # fund: the id has to be present
+    logger.info('Sanify Check for fund')
+    fund_sane = True
+    cur.execute('SELECT * from fund WHERE NOT EXISTS (select 1 from asset where fund.id = asset.id)')
+    for zombie in cur.fetchall():
+        logger.error('fund %s referrs to a non-existant asset' % zombie['id'])
+        fund_sane = False
+    if fund_sane:
+        logger.info('Sanity Check for fund passed')
+    checked_tables.append('fund')
+    # position: the asset_id has to exist
+    logger.info('Sanity Check for position')
+    position_sane = True
+    cur.execute('SELECT * from position WHERE NOT EXISTS (select 1 from asset where position.asset_id = asset.id)')
+    for zombie in cur.fetchall():
+        position_sane = False
+        logger.error('position %s referrs to a non-existant asset %s' % (zombie['id'], zombie['asset_id']))
+    if position_sane:
+        logger.info('Sanity Check for position passed')
+    checked_tables.append('position')
         
 except db.Error, e: 
     logger.error("Error %s" % e.args[0])
